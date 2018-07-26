9f19f870545d8f37789eca52ef23cbe28a96589c
cherranzp <41328891+cherranzp@users.noreply.github.com>
2018-07-26 13:25:42 +0200

    Update ObtainImages.m

--- NYU_depth_v2/ObtainImages.m
+++ NYU_depth_v2/ObtainImages.m
@@ -1,18 +1,18 @@
-    load('./NYUdepth/Code/nyu_depth_v2_labeled.mat');
+    load('./NYUdepth/code/nyu_depth_v2_labeled.mat');
 
     %%RGB images
     for i=1:size(rawRgbFilenames,1)
-        folder = fullfile('NYUdepth/Images2', strtok(rawRgbFilenames{i,1},'/'))
+        folder = fullfile('NYUdepth/Images', strtok(rawRgbFilenames{i,1},'/'))
         if ~exist(folder, 'dir')
             mkdir(folder)
         end
-        imwrite(uint8(images(:,:,:,i)), sprintf('NYUdepth/Images2/%s.png', rawRgbFilenames{i,1}(1:end-4)));
+        imwrite(uint8(images(:,:,:,i)), sprintf('NYUdepth/Images/%s.png', rawRgbFilenames{i,1}(1:end-4)));
     end
     
     %%Annotations for 40 classes 
-    addpath(genpath('NYUdepth/Code/indoor_scene_seg_sup'))
+    addpath(genpath('NYUdepth/code/indoor_scene_seg_sup'))
     for i=1:size(rawRgbFilenames,1)
-        folder = fullfile('NYUdepth/Annotations402', strtok(rawRgbFilenames{i,1},'/'))
+        folder = fullfile('NYUdepth/Annotations40', strtok(rawRgbFilenames{i,1},'/'))
         if ~exist(folder, 'dir')
             mkdir(folder)
         end
@@ -20,7 +20,7 @@
         str_labels = str_labels-1;
         str_labels(str_labels == -1) = 255;
         
-        imwrite(uint8(str_labels), sprintf('NYUdepth/Annotations402/%s.png', rawRgbFilenames{i,1}(1:end-4)));
+        imwrite(uint8(str_labels), sprintf('NYUdepth/Annotations40/%s.png', rawRgbFilenames{i,1}(1:end-4)));
    end
 
 
